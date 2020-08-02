916cb5c57f299e68b62b908fc40ff5280717c654
itaymish <62606669+itaymish@users.noreply.github.com>
2020-08-02 20:27:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function std() {
+  document.getElementById("p").innerHTML = document.getElementById("std").value*0.293512;
+}
+function dts() {
+  document.getElementById("p").innerHTML = document.getElementById("dts").value*3.40702;
+}
